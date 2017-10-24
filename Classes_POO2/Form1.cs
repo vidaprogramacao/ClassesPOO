98b7543b441b691114963c88290617db0ba8916b
Sthe Soares <sthedeveloper@gmail.com>
2017-10-24 15:50:13 -0300

    Aula - Programação Orientada a Objetos C# - Criando Aplicação - Classe e Métodos
    Aula - Programação Orientada a Objetos C# - Criando Aplicação - Classe e Métodos
    https://www.youtube.com/watch?v=r9G4Pg-i7PM

---
+++ Classes_POO2/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Classes_POO2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // instanciar a classe
+            Pessoa PessoaLinda = new Pessoa();
+            // passar o conteudo que vai ser digitado para as variaveis 
+            PessoaLinda.setNome(textBox1.Text);
+            try
+            {
+                PessoaLinda.setIdade(int.Parse(textBox2.Text));
+                // obter o nome e da idade ↓
+                string xnome = PessoaLinda.getNome();
+                int xidade = PessoaLinda.getIdade();
+                string strsaida = " Nome da Pessoa : " + xnome.ToUpper() + "\n\n" + "Idade :" + xidade.ToString();
+                MessageBox.Show(strsaida, " ****Resultado****",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show(erro.Message+"\n\nTente Novamente ;(" , "****** ERRO DE ENTRADA ***",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+            }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox1.Focus();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string texto = "Deseja Sair da Aplicação?";
+            string titulo = "*** FINALIZANDO APLICAÇÃO";
+            if (MessageBox.Show(texto, titulo,
+                MessageBoxButtons.YesNo,
+               MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+        
+    }
+}
---
+++ Classes_POO2/Pessoa.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_POO2
+{
+    class Pessoa
+    {
+        // atributos da minha classe ↓
+        private string nome;
+        private int idade;
+
+        // metodos get e set :D
+        // get vai pegar
+        // set vai gravar 
+
+        public void setNome(string nome)
+        {
+            this.nome = nome;
+        }
+
+        public void setIdade(int idade)
+        {
+            this.idade = idade;
+        }
+
+        // get 
+
+        public string getNome()
+        {
+            return this.nome;
+        }
+
+        public int getIdade()
+        {
+            return this.idade;
+        }
+    }
+}
